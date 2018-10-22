553dd2a3db64f73d475b92f77ae620a9c541b188
Zero Tang <zero.tangptr@gmail.com>
2018-10-22 04:17:44 -0400

    Create readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # NoirVisor
+
+NoirVisor - Hardware-Accelerated Hypervisor solution with support to complex functions and purposes and nested virtualization.
+
+# Introduction
+NoirVisor is a hardware-accelerated hypervisor (a.k.a VMM, Virtual Machine Monitor) with support to complex functions and purposes. It is designed to support processors based on x86 architecture with hardware-accelerated virtualization feature. For example, Intel processors supporting Intel VT-x or AMD processors supporting AMD-V meet the requirement. By designation, NoirVisor determines the processor manufacturer and selects the function core.
+
+# Processor Requirement
+Intel Processors based on Intel 64 and IA-32 Architecture, with support to Intel VT-x. Intel EPT is prefered, but not required.
+AMD Processors based on AMD64 Architecture, with support to AMD-V. Nested Paging is prefered, but not required.
+Other processors based on x86 architecture may be supported in future.
+
+# Nested Virtualization
+NoirVisor is developed in highest focus on nested virtualization. It is not currently supported, but will be developed in future.
+
+# Announcement to all contributors
+NoirVisor is coded in the C programming language and the assembly since it is procedure-oriented designed.
+DO NOT PROVIDE CODES WITH C++ WHICH INVOLVES THE NoirVisor Core IN YOUR PULL-REQUEST!
+
+# Build
+To build a kernel-mode driver on Windows, you should install Windows Driver Kit 7.1.0 to default path on C disk. Then run the provided batch file to build it.
+
+# Supported Platforms
+NoirVisor is designed to be cross-platform. It can be built to a kernel-mode component of an operating system, or even as a software with bootstrap running on bare-metal.
+Currently, NoirVisor supports the Windows Operating System higher than Windows Server 2000, running as a kernel-mode driver.
+If there is already a hypervisor running in the system, make sure it supports native virtualization nesting.
+
+# License
+All the codes with NoirVisor are licensed under MIT License.
---
+++ compchk_win7x64.bat
@@ -0,0 +1,35 @@
+@echo off
+set ddkpath=C:\WinDDK\7600.16385.1\bin\x86
+set incpath=C:\WinDDK\7600.16385.1\inc
+set libpath=C:\WinDDK\7600.16385.1\lib
+set binpath=.\bin\compchk_win7x64
+set objpath=.\bin\compchk_win7x64\Intermediate
+
+title Compiling NoirVisor
+echo Project: NoirVisor
+echo Platform: 64-Bit Windows
+echo Powered by zero.tangptr@gmail.com
+pause
+
+echo ============Start Compiling============
+echo Compiling Windows Driver Framework...
+%ddkpath%\amd64\cl.exe .\src\booting\windrv\driver.c /I"%incpath%\crt" /I"%incpath%\api" /I"%incpath%\ddk" /Zi /nologo /W3 /WX /Od /Oy- /D"_AMD64_" /D"_M_AMD64" /D"_WIN64" /D "_NDEBUG" /D"_UNICODE" /D "UNICODE" /Zc:wchar_t /Zc:forScope /FAcs /Fa"%objpath%\driver.cod" /Fo"%objpath%\driver.obj" /Fd"%objpath%\vc90.pdb" /GS- /Gz /TC /c /errorReport:queue
+
+echo Compiling Core Engine of Intel VT-x...
+%ddkpath%\amd64\cl.exe .\src\vt_core\vt_main.c /I".\src\include" /Zi /nologo /W3 /WX /Od /Oy- /D"_msvc" /D"_amd64" /Zc:wchar_t /Zc:forScope /FAcs /Fa"%objpath%\vt_main.cod" /Fo"%objpath%\vt_main.obj" /Fd"%objpath%\vc90.pdb" /GS- /Gz /TC /c /errorReport:queue
+
+echo Compiling Core Engine of AMD-V...
+%ddkpath%\amd64\cl.exe .\src\svm_core\svm_main.c /I".\src\include" /Zi /nologo /W3 /WX /Od /Oy- /D"_msvc" /D"_amd64" /Zc:wchar_t /Zc:forScope /FAcs /Fa"%objpath%\svm_main.cod" /Fo"%objpath%\svm_main.obj" /Fd"%objpath%\vc90.pdb" /GS- /Gz /TC /c /errorReport:queue
+
+echo Compiling Core of Cross-Platform Framework (XPF)...
+%ddkpath%\amd64\cl.exe .\src\xpf_core\windows\windows.c /I"%incpath%\crt" /I"%incpath%\api" /I"%incpath%\ddk" /Zi /nologo /W3 /WX /Od /Oy- /D"_AMD64_" /D"_M_AMD64" /D"_WIN64" /D "_NDEBUG" /D"_UNICODE" /D "UNICODE" /Zc:wchar_t /Zc:forScope /FAcs /Fa"%objpath%\windows.cod" /Fo"%objpath%\windows.obj" /Fd"%objpath%\vc90.pdb" /GS- /Gz /TC /c /errorReport:queue
+
+%ddkpath%\amd64\ml64.exe /W3 /WX /Zf /Zd /Fo"%objpath%\vt_hv64.obj" /c /nologo .\src\xpf_core\windows\vt_hv64.asm
+
+echo ============Start Linking============
+%ddkpath%\amd64\link.exe "%objpath%\driver.obj" "%objpath%\vt_main.obj" "%objpath%\svm_main.obj" "%objpath%\windows.obj" "%objpath%\vt_hv64.obj" "%objpath%\ver64.res" /LIBPATH:"%libpath%\win7\amd64" "ntoskrnl.lib" /NOLOGO /DEBUG /PDB:"%objpath%\NoirVisor.pdb" /OUT:"%binpath%\NoirVisor.sys" /SUBSYSTEM:NATIVE /Driver /ENTRY:"NoirDriverEntry" /Machine:X64 /ERRORREPORT:QUEUE
+
+echo ============Start Signing============
+%ddkpath%\signtool.exe sign /v /f .\ztnxtest.pfx /t http://timestamp.globalsign.com/scripts/timestamp.dll %binpath%\NoirVisor.sys
+
+pause
